853bc9ffdf13f3060dd8a82edeb9aeb1743afb2e
staceynapier <staceynapier86@gmail.com>
2017-08-30 17:59:04 +0100

    initial commit

---
+++ array_tasks.js
@@ -0,0 +1,40 @@
+var arrayTasks = {
+
+	concat: function (arr1, arr2) {
+		return arr1.concat(arr2);
+	},
+
+	insertAt: function (arr, itemToAdd, index) {
+		arr.splice(index, 0, itemToAdd);
+		return arr;
+	},
+
+	square: function (arr) {
+		var squared = arr.map( function( num ) {
+			return (num ** num);
+		} )
+	},
+
+	// sum: function (arr) {
+		
+	// },
+
+	// findDuplicates: function (arr) {
+		
+	// },
+
+	// removeAndClone: function (arr, valueToRemove) {
+		
+	// },
+
+	// findIndexesOf: function (arr, itemToFind) {
+		
+	// },
+
+	// sumOfAllEvenNumbersSquared: function (arr) {
+		
+	// }
+
+}
+
+module.exports = arrayTasks
